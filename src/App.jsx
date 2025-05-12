193dec36e95e71f80b3ba6f88bc0a0dfc938be2e
ch4mi2 <chamithu4@gmail.com>
2025-05-12 13:02:13 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ARScene from "./components/ARScene";
+
+function App() {
+  return (
+    <div style={{ position: "absolute", top: 0, left: 0, padding: 10 }}>
+      <h2 style={{ color: "white" }}>React is running ✅</h2>
+    </div>
+  );
+}
+
+export default App;
+
