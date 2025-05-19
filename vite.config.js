040f3d1a693c5e6c9058073b41ac22d3cdf6e36a
ch4mi2 <chamithu4@gmail.com>
2025-05-19 23:16:45 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // https: {
-    //   key: fs.readFileSync('./ssl-cert/key.pem'),
-    //   cert: fs.readFileSync('./ssl-cert/cert.pem'),
-    // },
-    host: true, // This exposes the server to your network
-    port: 5173, // Optional: specify a port
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl-cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl-cert/cert.pem')),
+    },
+    host: true,
+    port: 5173,
     allowedHosts: true,
   },
 });
